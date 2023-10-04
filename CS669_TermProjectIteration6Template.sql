65ab55f20a4bc7f01a4febce694c4cf7cc5de803
selina <66767001+selinaliu0626@users.noreply.github.com>
2023-10-04 11:04:43 -0700

    the latest version for this project

---
+++ CS669_TermProjectIteration6Template.sql
@@ -0,0 +1,17 @@
+--TABLES
+--Replace this with your table creations (including the history table).
+
+--SEQUENCES
+--Replace this with your sequence creations (including the one for the history table).
+
+--INDEXES
+--Replace this with your index creations.
+
+--STORED PROCEDURES
+--Replace this with your stored procedure definitions.
+
+--TRIGGERS
+--Replace this with your history table trigger.
+
+--QUERIES
+--Replace this with your queries (including any queries used for data visualizations).
